4428dd355ee5ce2d589a8f4b195e7f51d78a165e
lhm <hanmingli1993@163.com>
2018-05-03 09:40:13 +0800

    app

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
-import logging
-
-logging.basicConfig(level=logging.INFO)
-
+import logging;logging.basicConfig(level=logging.INFO)  # 设置logging，还可以配置日志文件，日志格式等
 import asyncio
 from aiohttp import web
 
 
 def index(request):
-    return web.Response(body=b'<h1>okkkk</h1>')
+    return web.Response(body='<html><h1>okkkk</h1></html>', content_type='text/html')
 
 
 @asyncio.coroutine
 def init(loop):
     app = web.Application(loop=loop)
-    app.router.add_router('GET', '/', index)
+    app.router.add_route('GET', '/', index)
     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000')
     return srv
