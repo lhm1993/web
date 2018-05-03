4b298df78356f8aaf7bcfa727f4657be190a2153
lhm <hanmingli1993@163.com>
2018-05-03 18:46:20 +0800

    coroutine problem

--- test/test1.py
+++ test/test1.py
@@ -1,6 +1,7 @@
 from www import orm_ref
 from www.models import User
 import asyncio
+import sys
 
 async def test():
 
@@ -8,12 +9,19 @@ async def test():
     await orm_ref.create_pool(loop=loop, port=3306, user='root', password='123', db='awesome')
 
     #没有设置默认值的一个都不能少
-    u = User(name='dflhuang23', email='dflhuang@qq.com23', passwd='012323', image='about:blank2', id='1203')
+    u = User(name='dflhuang23', email='dflhuang@qq.com3', passwd='012323', image='about:blank2', id='3372')
 
     await u.save()
+    data = User.findAll()
+    with open("./tt", "w") as fd:
+        fd.write(str(data))
+
+
 # 获取EventLoop:
 loop = asyncio.get_event_loop()
 
 #把协程丢到EventLoop中执行
 loop.run_until_complete(test())
-
+loop.close()
+if loop.is_closed():
+    sys.exit(0)
--- test/test2.py
+++ test/test2.py
@@ -1,10 +1,12 @@
 from www.models import User
+import www.orm_ref
+import asyncio
 
 
 def get():
     data = User.findAll()
-    while data.next():
-        yield data
+    for d in data:
+        print(d)
+
 if __name__ == '__main__':
-    get()
     get()
