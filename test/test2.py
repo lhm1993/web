613c56c964471a5a43849d45a891c25ab97e0272
lhm <hanmingli1993@163.com>
2018-05-04 10:32:31 +0800

    app

--- test/test2.py
+++ test/test2.py
@@ -1,12 +1,21 @@
+from www import orm_ref
 from www.models import User
-import www.orm_ref
 import asyncio
+import sys
 
+async def test():
 
-def get():
-    data = User.findAll()
+    #创建连接池,里面的host,port,user,password需要替换为自己数据库的信息
+    await orm_ref.create_pool(loop=loop, port=3306, user='root', password='123', db='awesome')
+    # 如果使用可协程，则每一层都要使用协程
+    data = await User.findAll()
     for d in data:
         print(d)
 
-if __name__ == '__main__':
-    get()
+
+# 获取EventLoop:
+loop = asyncio.get_event_loop()
+
+#把协程丢到EventLoop中执行
+loop.run_until_complete(test())
+loop.close()
